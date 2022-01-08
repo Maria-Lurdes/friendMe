0cebbd6e642142e9ba7bd1fec81414337f650571
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-01-08 18:05:20 +0200

    done: testing form on sign in

--- src/app/sign-in/sign-in.component.spec.ts
+++ src/app/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
@@ -8,6 +10,7 @@ describe('SignInComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ SignInComponent ]
     })
     .compileComponents();
@@ -21,5 +24,16 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create from with 2 controls ',  () => {
+    expect(component.signInForm.contains('email')).toBeTruthy();
+    expect(component.signInForm.contains('password')).toBeTruthy();
+  });
+
+  it('should mark email as invalid if value will be empty ', () => {
+    const control = component.signInForm.get('email');
+    control.setValue('');
+    expect(control.valid).toBeFalse();
   });
 });
