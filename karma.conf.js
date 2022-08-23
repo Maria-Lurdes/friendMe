16bcd26f4306c1a74166b23d13e444ffaeb997f3
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-08-23 20:09:51 +0300

    done: changed to chrome

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_ERROR,
     autoWatch: false,
-    browsers: ["ChromeHeadlessCI"],
+    browsers: ["Chrome"],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: "ChromeHeadless",
