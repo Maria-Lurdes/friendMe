21ec76c296db43174dc30ecfd1d13d2374a5dfc2
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-09-08 17:26:36 +0300

    done: favourites add

--- src/app/dashboard/favourites-pets/favourites-pets.component.ts
+++ src/app/dashboard/favourites-pets/favourites-pets.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Post } from "../../shared/interfaces";
 import { PostService } from "../../shared/services/post.service";
 import { AuthService } from "../../shared/services/auth.service";
-import { AlertService } from "../../shared/services/alert.service";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getDownloadURL, getStorage, listAll, ref } from "firebase/storage";
 
 @Component({
   selector: "app-favourites-pets",
@@ -13,6 +11,7 @@ import { getDownloadURL, getStorage, listAll, ref } from "firebase/storage";
 })
 export class FavouritesPetsComponent implements OnInit {
   posts: Post[] = [];
+  allPetsPosts: Post[] = [];
   favouritesList: string[] = [];
   userId: string = "";
   auth = getAuth();
@@ -21,80 +20,68 @@ export class FavouritesPetsComponent implements OnInit {
 
   constructor(
     private postService: PostService,
-    private authService: AuthService,
-    private alert: AlertService
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
+    this.getAllPets();
+    this.getFavouritesPosts();
     this.getChosenPets();
     this.getFavouritePetsList();
     this.getUserId();
+  }
+
+  getAllPets() {
+    this.postService.getAll();
   }
 
   getFavouritePetsList() {
     this.authService.favouritesPetsList.subscribe((list) => {
       this.showLoader = !!list.length;
       this.favouritesList = list;
-      this.posts = [];
-      let favouritePosts = [];
-      let countIndex = 0;
-      const promises = this.favouritesList.map((id) =>
-        this.postService.getById(id)
-      );
-      // @ts-ignore
-      Promise.allSettled(promises).then((results) => {
-        results.forEach((result) => {
-          if (result.status === "fulfilled") {
-            result.value.subscribe((val) => {
-              if (val.name) {
-                let petPost = { ...val };
-                if (countIndex === 0) {
-                  petPost = { ...petPost, color: "green" };
-                } else {
-                  if (
-                    favouritePosts[favouritePosts.length - 1].color === "green"
-                  ) {
-                    petPost = { ...petPost, color: "orange" };
-                  } else if (
-                    favouritePosts[favouritePosts.length - 1].color === "orange"
-                  ) {
-                    petPost = { ...petPost, color: "blue" };
-                  } else if (
-                    favouritePosts[favouritePosts.length - 1].color === "blue"
-                  ) {
-                    petPost = { ...petPost, color: "green" };
-                  }
-                }
-                countIndex++;
-                favouritePosts.push(petPost);
-                this.posts = favouritePosts;
-              }
-            });
-          }
-        });
-      });
-      this.getImagesUrl();
+      this.findFavouritesInfo();
     });
   }
 
-  getImagesUrl() {
-    let fireStorage = getStorage();
-    const listRef = ref(fireStorage, `pets-avatars`);
-    listAll(listRef)
-      .then((res) => {
-        res.items.forEach((itemRef) => {
-          getDownloadURL(itemRef).then((url) => {
-            let index = this.posts.findIndex(
-              (post) => post.id === itemRef.name
-            );
-            if (index >= 0) this.posts[index].avatar = url;
-          });
-        });
+  findFavouritesInfo() {
+    let filteredPosts = this.allPetsPosts.filter((post) =>
+      this.favouritesList.includes(post.id)
+    );
+    this.posts = this.getPostsByColor(filteredPosts);
+    this.showLoader = false;
+  }
+
+  getFavouritesPosts() {
+    this.postService.petPostsArray.subscribe(
+      (currentPosts) => {
+        this.allPetsPosts = currentPosts;
+        this.findFavouritesInfo();
+      },
+      () => {
         this.showLoader = false;
-      })
-      .catch(() => {
-        this.alert.danger("Smth went wrong, try again later");
-      });
+      }
+    );
+  }
+
+  getPostsByColor(posts) {
+    let updatedPosts = [];
+    posts.forEach((item, index) => {
+      let postItem = { ...item };
+      if (index === 0) {
+        postItem = { ...postItem, color: "green" };
+      } else {
+        if (updatedPosts[updatedPosts.length - 1].color === "green") {
+          postItem = { ...postItem, color: "orange" };
+        } else if (updatedPosts[updatedPosts.length - 1].color === "orange") {
+          postItem = { ...postItem, color: "blue" };
+        } else if (updatedPosts[updatedPosts.length - 1].color === "blue") {
+          postItem = { ...postItem, color: "green" };
+        }
+      }
+      updatedPosts.push(postItem);
+    });
+
+    return updatedPosts;
   }
 
   getChosenPets() {
