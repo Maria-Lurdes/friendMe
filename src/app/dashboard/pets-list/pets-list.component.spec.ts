b434ad55e3740e852abf741afb859cb198cb6201
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-09-23 17:51:55 +0300

    done: added initializeApp(firebase);

--- src/app/dashboard/pets-list/pets-list.component.spec.ts
+++ src/app/dashboard/pets-list/pets-list.component.spec.ts
@@ -4,6 +4,8 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { AlertService } from "../../shared/services/alert.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { FilterPipe } from "../../shared/filter.pipe";
+import { initializeApp } from "firebase/app";
+import { firebase } from "../../../environments/environment";
 
 describe("PetsListComponent", () => {
   let component: PetsListComponent;
@@ -18,6 +20,7 @@ describe("PetsListComponent", () => {
   }));
 
   beforeEach(() => {
+    initializeApp(firebase);
     fixture = TestBed.createComponent(PetsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
