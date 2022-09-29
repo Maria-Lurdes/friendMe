a64151b879617e911d7418b92cb718a1e6033fbd
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-09-29 18:27:11 +0300

    done: enableIndexedDbPersistence added catch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -90,6 +90,18 @@ export class AppModule {
   constructor() {
     const app = initializeApp(firebase);
     const db = getFirestore(app);
-    enableIndexedDbPersistence(db);
+    enableIndexedDbPersistence(db).catch((err) => {
+      if (err.code == "failed-precondition") {
+        console.log("failed-precondition");
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a a time.
+        // ...
+      } else if (err.code == "unimplemented") {
+        console.log("unimplemented");
+        // The current browser does not support all of the
+        // features required to enable persistence
+        // ...
+      }
+    });
   }
 }
