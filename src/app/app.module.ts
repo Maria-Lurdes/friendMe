8c68584b7ee8704f5de02dcddadd94eb23a83ccb
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-10-04 11:19:32 +0300

    done: enable cross-origin isolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -90,18 +90,14 @@ export class AppModule {
   constructor() {
     const app = initializeApp(firebase);
     const db = getFirestore(app);
-    enableIndexedDbPersistence(db).catch((err) => {
-      if (err.code == "failed-precondition") {
-        console.log("failed-precondition");
-        // Multiple tabs open, persistence can only be enabled
-        // in one tab at a a time.
-        // ...
-      } else if (err.code == "unimplemented") {
-        console.log("unimplemented");
-        // The current browser does not support all of the
-        // features required to enable persistence
-        // ...
-      }
-    });
+    enableIndexedDbPersistence(db)
+      .then(() => console.log("enableIndexedDbPersistence"))
+      .catch((err) => {
+        if (err.code == "failed-precondition") {
+          console.log("failed-precondition");
+        } else if (err.code == "unimplemented") {
+          console.log("unimplemented");
+        }
+      });
   }
 }
