3087bf75aeb1e5e57562c0c56d1c7d8314243e81
Maria-Lurdes <lurdesdavinci@gmail.com>
2022-10-10 17:09:13 +0300

    done: test for get request

---
+++ src/app/shared/services/post.service.spec.ts
@@ -0,0 +1,48 @@
+import { PostService } from "./post.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { environment } from "../../../environments/environment";
+
+describe("PostService", () => {
+  let httpMock: HttpTestingController;
+  let testService: PostService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PostService],
+    });
+
+    testService = TestBed.inject(PostService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it("get pet by id", () => {
+    const post = {
+      age: 2,
+      description:
+        "Tolsona also known as Tiny which is what she response to, is a sweet sweet girl...she is very affectionate. She gets along great with all dogs. She enjoys playing, rough housing , and chilling with you. Tolsona is NOT A CAT girl. She has gone after ours many times. Does great on car rides. Walks calmly on a leash. House and kennel trained, great with kids.",
+      name: "Tolsona",
+      sex: "female",
+      type: "dog",
+      weight: 7,
+    };
+
+    testService.getById("-N9f_QFh_emE9IVfX7jf").subscribe((res) => {
+      expect(res.name).toEqual(post.name);
+      expect(res.description).toEqual(post.description);
+      expect(res.age).toEqual(post.age);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.fvDbUrl}/posts/-N9f_QFh_emE9IVfX7jf.json`
+    );
+    expect(req.request.method).toEqual("GET");
+    req.flush(post);
+
+    httpMock.verify();
+  });
+});
